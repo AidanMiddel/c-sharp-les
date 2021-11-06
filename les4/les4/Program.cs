7f1242845ac300070058a2ea2c74ee5a49fed072
AidanMiddel <70641287+AidanMiddel@users.noreply.github.com>
2021-11-06 16:16:43 +0100

    les 4

---
+++ les4/les4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        private class mooietent
+        {
+
+        }
+
+        private class Tent
+        {
+            private mooietent[][] mooietent;
+
+            public Tent(int width, int height)
+            {
+                this.mooietent = new mooietent[height][];
+
+                for (int i = 0; i < height; i++)
+                {
+                    this.mooietent[i] = new mooietent[width];
+
+                    for (int j = 0; j < width; j++)
+                    {
+                        this.mooietent[i][j] = new mooietent();
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Tent tent = new Tent(2, 2);
+        }
+    }
+}
